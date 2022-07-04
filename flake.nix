0ecb391416d83e071ca57eed578ef88bc2b97100
Chua Hou <human+github@chuahou.dev>
2022-07-04 19:07:57 +0800

    flake: init with haskell.nix
    - nix shell allows both cabal and stack builds.
    - nix build should work as well.
    - Stack runs in nix mode.
    - Resolver is upgraded to get around glibc mismatches between the
      snapshot and haskell.nix's nixpkgs for it.
    - Version constraints removed on package.yaml to leave resolving up to
      the snapshot.
    - Upgrade required an additional pragma in src/Model.hs.
    - Clean up helpers like .envrc and .dir-locals.el, as well as .gitignore.

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  };
+
+  # haskell.nix binary cache
+  nixConfig = {
+    extra-substituters = "https://cache.iog.io";
+    extra-trusted-public-keys = "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=";
+  };
+
+  outputs = inputs@{ self, nixpkgs, haskellNix, ... }:
+  let
+    system = "x86_64-linux";
+    compiler-nix-name = "ghc902";
+    compiler = pkgs.haskell-nix.compiler.${compiler-nix-name};
+    overlays = [
+      haskellNix.overlay
+      (self: super: {
+        hello-yesod = self.haskell-nix.project' {
+          src = ./.;
+          inherit compiler-nix-name;
+        };
+      })
+    ];
+    pkgs = import nixpkgs {
+      inherit system overlays;
+      inherit (haskellNix) config;
+    };
+    flake = pkgs.hello-yesod.flake {};
+
+  in flake // {
+    inherit pkgs;
+    defaultPackage.${system} = flake.packages."hello-yesod:exe:hello-yesod";
+    devShell.${system} = self.devShells.${system}.stack;
+    devShells.${system} = {
+      # For stack's use when building.
+      stack = pkgs.mkShell {
+        buildInputs = with pkgs; [ compiler git nix stack zlib ];
+        NIX_PATH = "nixpkgs=${pkgs.path}";
+        LC_ALL = "C.UTF-8";
+      };
+      # Development shell for use with `nix develop`.
+      dev = pkgs.hello-yesod.shellFor {
+        tools = { cabal = {}; haskell-language-server = {}; };
+        buildInputs = with pkgs; [ hpack stack ];
+        exactDeps = true;
+      };
+    };
+  };
+}
